c6bfb00b83c43449239baca4b0f3217cd6b62a5e
faizan ahemad <fahemad3@gmail.com>
2016-08-09 00:04:03 +0530

    Improving startup screen-Added ProgressBar

--- src/main/scala/io/faizan/Main.scala
+++ src/main/scala/io/faizan/Main.scala
@@ -16,16 +16,21 @@ object Main extends ServerApp {
 
   override def server(args: List[String]): Task[Server] = {
     val splash = new SplashDisplay("DNS Server")
-    splash.render("Starting Http Server")
+    splash.render("Starting Http Server",10)
     val reqRunningStatus = GET(uri("http://localhost:8080/admin/status"))
     val afterRunCall = client.expect[String](reqRunningStatus)
                        .map((resp) => Utils.fromJson[ServerStatus](resp))
                        .map {
-                              case e: ServerStatus if e == ServerStatus(ServerStatus.RUNNING) => e
+                              case e: ServerStatus if e == ServerStatus(ServerStatus.RUNNING) =>
+                                splash.render("Server Status: "+e.status,70)
+                                Thread.sleep(500)
+                                e
                               case f: ServerStatus =>
+                                splash.render("Unable to start. Terminating Program",0)
+                                Thread.sleep(1000)
                                 System.exit(1)
                                 f
-                            }.after(10000 millisecond)
+                            }.after(8000 millisecond)
     val req = PUT(uri("http://localhost:8080/admin/start"))
     val startCall = client.expect[String](req).map((resp) => Utils.fromJson[ServerStatus](resp))
                     .map {
@@ -34,16 +39,16 @@ object Main extends ServerApp {
                              System.exit(1)
                              f
                          }.map((s)=>{
-      splash.render("Checking for Server Run")
+      splash.render("Pinging Server for Status",55)
       afterRunCall.run
-      splash.render("Go to http://localhost:8080/")
-      Thread.sleep(1000)
+      splash.render("Go to http://localhost:8080/",100)
+      Thread.sleep(1500)
       splash.stop()
     })
 
     val startTask = BlazeBuilder.bindHttp(8080)
                     .mountService(httpServer.router).start.map(s => {
-      splash.render("Starting DNS Server")
+      splash.render("Starting DNS Server",30)
       startCall.run
       s
     })
--- src/main/scala/io/faizan/SplashDisplay.scala
+++ src/main/scala/io/faizan/SplashDisplay.scala
@@ -2,27 +2,23 @@ package io.faizan
 
 import java.awt._
 import java.awt.event._
+import java.awt.geom.Rectangle2D
 
 
 class SplashDisplay(title: String) extends Frame(title) with ActionListener {
-  private var dotCounter = 0
   private var splash = Option.empty[SplashScreen]
   private var g = Option.empty[Graphics2D]
+  private var progressArea = Option.empty[Rectangle2D.Double]
 
 
   private def renderSplashFrame(display: String) {
     g.foreach((gr) => {
       gr.setComposite(AlphaComposite.Clear)
-      gr.fillRect(140, 180, 220, 80)
+      gr.fillRect(100, 180, 230, 80)
       gr.setPaintMode()
       gr.setColor(Color.BLACK)
-      gr.drawString(display + getDots, 140, 230)
+      gr.drawString(display + "...", 100, 230)
     })
-  }
-
-  private def getDots: String = {
-    dotCounter += 1
-    (1 to dotCounter % 5).foldLeft[String](" .") { (z, i) => z + "." }
   }
 
   private def createMenuBar = {
@@ -36,15 +32,54 @@ class SplashDisplay(title: String) extends Frame(title) with ActionListener {
     mb.add(m1)
   }
 
+  private def splashProgress(pct: Int) {
+    splash.foreach(mySplash=>{
+      g.foreach(splashGraphics=>{
+        val splashProgressArea = progressArea.get
+        splashGraphics.setPaint(Color.WHITE)
+        splashGraphics.fill(splashProgressArea)
+        // draw an outline
+        splashGraphics.setPaint(Color.YELLOW)
+        splashGraphics.draw(splashProgressArea)
+        // Calculate the width corresponding to the correct percentage
+        val x = splashProgressArea.getMinX.toInt
+        val y = splashProgressArea.getMinY.toInt
+        val wid = splashProgressArea.getWidth.toInt
+        val hgt = splashProgressArea.getHeight.toInt
+        var doneWidth = pct * wid / 100.0f.round
+        doneWidth = Math.max(0, Math.min(doneWidth, wid - 1)) // limit 0-width
+        // fill the done part one pixel smaller than the outline
+        splashGraphics.setPaint(Color.BLUE)
+        splashGraphics.fillRect(x, y + 1, doneWidth, hgt - 1)
+        // make sure it's displayed
+        mySplash.update()
+      })
+    })
+  }
+
   private def init = {
     splash = Option(SplashScreen.getSplashScreen)
-    splash.foreach((s) => g = Option(s.createGraphics))
+    splash.foreach((s) => {
+      g = Option(s.createGraphics).map(g=>{
+        val font = new Font("Dialog", Font.ROMAN_BASELINE, 18)
+        g.setFont(font)
+        g
+      })
+    })
+    progressArea = splash.map(mySplash=>{
+      val ssDim = mySplash.getSize
+      val height = ssDim.height
+      val width = ssDim.width
+      val splashProgressArea = new Rectangle2D.Double(width * .05, height * .88, width*0.90, 3)
+      splashProgressArea
+    })
     setSize(300, 200)
     setLayout(new BorderLayout)
     createMenuBar
   }
 
-  private def renderSplash(text: String): Unit = {
+  private def renderSplash(text: String, pct:Int): Unit = {
+    splashProgress(pct)
     renderSplashFrame(text)
     splash.get.update()
   }
@@ -53,7 +88,7 @@ class SplashDisplay(title: String) extends Frame(title) with ActionListener {
     splash.get.close()
   }
 
-  private def renderText(text: String): Unit = {
+  private def renderText(text: String, pct:Int): Unit = {
     println(text)
   }
 
