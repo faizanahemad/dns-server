f3e745deee8ea3d85fb50a3c7fc1fba84ce6e7c4
faizan ahemad <fahemad3@gmail.com>
2016-08-22 00:44:13 +0530

    Improved start time

--- src/main/scala/io/faizan/Main.scala
+++ src/main/scala/io/faizan/Main.scala
@@ -10,34 +10,43 @@ import org.http4s.dsl._
 import org.http4s.server.blaze.BlazeBuilder
 import org.http4s.server.{Server, ServerApp}
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.util.Try
 import scalaz.concurrent.Task
 
 object Main extends SuperUserApplication with ServerApp {
+  import scala.concurrent.ExecutionContext.Implicits.global
 
+  val sleepBase = 300L
+  val splash = new SplashDisplay("DNS Server")
+  splash.render("Checking Sudo/Root Permissions",10)
+  val systemExit = (msg:String)=> {
+    splash.render(msg,10)
+    Thread.sleep(sleepBase*2)
+    splash.stop()
+    System.exit(1)
+  }
+  val sudoNeeded = Option(System.getProperty("sudo")).forall(s=> Try(s.toBoolean).getOrElse(true))
+  if (sudoNeeded && !isSuperUser) {
+    systemExit("Sudo/Root Perms Not present. Exiting")
+  }
+
+  splash.render("Starting DNS Server",20)
   val httpServer = new HttpServer
+
+  val dnsFuture = Future {
+                           httpServer.startServer
+                                    }
 
   override def server(args: List[String]): Task[Server] = {
 
     implicit val scheduledExecutorService = Executors.newScheduledThreadPool(Runtime.getRuntime.availableProcessors())
-    val sleepBase = 40L
-    val splash = new SplashDisplay("DNS Server")
-    splash.render("Checking Sudo/Root Permissions",10)
-    val systemExit = (msg:String)=> {
-      splash.render(msg,10)
-      Thread.sleep(sleepBase*2)
-      splash.stop()
-      System.exit(1)
-    }
-    val sudoNeeded = Option(System.getProperty("sudo")).forall(s=> Try(s.toBoolean).getOrElse(true))
-    if (sudoNeeded && !isSuperUser) {
-      systemExit("Sudo/Root Perms Not present. Exiting")
-    }
+
 
 
     Thread.sleep(sleepBase)
-    splash.render("Starting Http Server",20)
+    splash.render("Starting Http Server",40)
 
 
     val reqRunningStatus = GET(uri("http://localhost:8080/admin/status"))
@@ -51,32 +60,20 @@ object Main extends SuperUserApplication with ServerApp {
                               case f: ServerStatus =>
                                 systemExit("Unable to start. Terminating Program")
                                 f
-                            }.after(6000 millisecond)
-
-
-    val reqStartServer = PUT(uri("http://localhost:8080/admin/start"))
-    val startCall = client.expect[String](reqStartServer)
-                    .timed(6000)
-                    .map((resp) => Utils.fromJson[ServerStatus](resp))
-                    .map {
-                           case e: ServerStatus if e == ServerStatus(ServerStatus.STARTING) => e
-                           case f: ServerStatus =>
-                             systemExit("Unable to start. Terminating Program")
-                             f
-                         }.map((s)=>{
-      splash.render("Pinging Server for Status",60)
-      afterRunCall.run
-      splash.render("Go to http://localhost:8080/",100)
-      Thread.sleep(sleepBase*3)
-      splash.stop()
-    }).timed(15000)
+                            }.after(1000 millisecond)
 
     val startTask = BlazeBuilder.bindHttp(8080)
                     .mountService(httpServer.router).start.map(s => {
-      splash.render("Starting DNS Server",40)
-      Try(startCall.run).recover{
-                                  case _=>systemExit("Unable to start. Terminating Program")
-                                }
+      dnsFuture.onComplete(v=>{
+        splash.render("Pinging Server for Status",60)
+        Try(afterRunCall.run).recover{
+                                       case _=>systemExit("Unable to start. Terminating Program")
+                                     }.map(f=>{
+          splash.render("Go to http://localhost:8080/",100)
+          Thread.sleep(sleepBase*2)
+          splash.stop()
+        })
+      })
       s
     })
     startTask
