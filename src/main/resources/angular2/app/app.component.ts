cd968575fc98219519747c7e7aa8f3787c2e533e
faizan ahemad <fahemad3@gmail.com>
2016-08-27 01:47:44 +0530

    Added App component changes and support for form validation

--- src/main/resources/angular2/app/app.component.ts
+++ src/main/resources/angular2/app/app.component.ts
@@ -2,20 +2,38 @@ import {Component, Injectable, OnInit} from "@angular/core";
 import './rxjs-extensions';
 import {Http, Response} from "@angular/http";
 import {Observable} from 'rxjs/Rx';
+import {Stats} from "./models/stats";
+import {stat} from "fs";
 @Component({
     selector: 'my-app',
     templateUrl: 'angular2/app/app.component.html'
 })
 @Injectable()
 export class AppComponent implements OnInit {
-    stats: Observable<Response>;
+    stats: Stats = new Stats("STOPPED","NA","NA");
     private url:string = "admin/stats";
+    private defaultStats:Stats = new Stats("STOPPED","NA","NA");
+    running:boolean = false;
     constructor(private http: Http) {}
-    private getStats(): Observable<Response> {
+    private getStats() {
         return Observable.interval(5000)
-            .switchMap(() => this.http.get(this.url)).map((r: Response) => r.json());
+            .switchMap(() => this.http.get(this.url)).map((r: Response) => {
+                if (r.ok) {
+                    let stat = r.json() as Stats;
+                    if (stat.status!="RUNNING") {
+                        this.running = false;
+                    } else {
+                        this.running = true;
+                    }
+                    return stat;
+                }
+                else {
+                    this.running = false;
+                    return this.defaultStats;
+                }
+            }).subscribe(stat => this.stats = stat)
     }
     ngOnInit(){
-        this.stats = this.getStats();
+        this.getStats();
     }
 }
---
+++ src/main/resources/angular2/app/models/stats.ts
@@ -0,0 +1,7 @@
+export class Stats {
+    constructor(public status: string,
+                public cache: string,
+                public store: string) {
+    }
+
+}
